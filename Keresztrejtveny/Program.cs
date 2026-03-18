fa29158c4a03da13513adcc44b561362a80b8783
Petrik Diak <diak@petrik.hu>
2026-03-18 08:56:54 +0100

    5. Oszlopok és sorok kiíratása

--- Keresztrejtveny/Program.cs
+++ Keresztrejtveny/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
 			KeresztrejtvenyRacs racs = new KeresztrejtvenyRacs("kr1.txt");
+
+			Console.WriteLine("5. feladat");
+			Console.WriteLine($"Sorok száma: {racs.SorokDb}");
+			Console.WriteLine($"Oszlopok száma: {racs.OszlopokDb}");
 		}
     }
 }
